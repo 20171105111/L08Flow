bbc7cc4f10019690a9d24ac1d4e457da95b3f53a
20171105111 <2604406667@qq.com>
2018-09-20 08:39:27 +0800

    输出偶数

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  L08Flow
+//
+//  Created by 徐涛 on 2018/9/20.
+//  Copyright © 2018年 徐涛. All rights reserved.
+//
+
+import Foundation
+for index in(0...100){
+    if index%2==0{
+    print(index)
+    }
+}
+
